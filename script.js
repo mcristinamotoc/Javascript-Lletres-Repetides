eee0d4064fd4e9f20c83615ee806bdd391c75b3e
Cristina M <mcristinamotoc@gmail.com>
2020-10-28 11:02:05 +0100

    Fase 2

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ var nombre = ["C", "R", "I", "S", "T", "I", "N", "A"];
 for (var i = 0; i < nombre.length; i++) {
     console.log(nombre[i]);
 }
+
+/* FASE 2:
+Al bucle, si la lletra és una vocal imprimeix a la consola: ‘He trobat la VOCAL: __’. Sinó, imprimeix: ‘He trobat la CONSONTANT: __’.
+Si trobes un número, mostra per pantalla: ‘Els noms de persones no contenen el número: __’ */
+
+var miNombre = "CRISTINA6";
+var vocal = "AEIOU"
+for (var i = 0; i < miNombre.length; i++) {
+    if (miNombre[i].match(/[AEIOU]/gi)) {
+        console.log("He trobat la VOCAL: " + miNombre[i])
+    } else if (!isNaN(miNombre[i])) {
+        console.log("Els noms de les persones no contenen el numero: " + miNombre[i])
+    } else {
+        console.log("He trobat la CONSONANTE: " + miNombre[i])
+    }
+}
