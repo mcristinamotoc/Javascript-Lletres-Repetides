2b0912cf0f7ffd92ebfb726e582809b3e9504125
Cristina M <mcristinamotoc@gmail.com>
2020-10-28 10:58:45 +0100

    Fase 1

---
+++ script.js
@@ -0,0 +1,10 @@
+//L’exercici consisteix en mostrar per consola quantes vegades apareix cada lletra del teu nom i cognoms amb diferents variants (fases).
+
+/* FASE 1:
+Crea un array amb el teu nom on cada posició correspongui a una lletra.
+Fes un bucle que recorri aquest array i mostri per consola cadascuna de les lletres. */
+
+var nombre = ["C", "R", "I", "S", "T", "I", "N", "A"];
+for (var i = 0; i < nombre.length; i++) {
+    console.log(nombre[i]);
+}
