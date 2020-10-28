4ba532a58ea2f44d62dcc72bbb34503eee4aadde
Cristina M <mcristinamotoc@gmail.com>
2020-10-28 11:07:37 +0100

    Fase 4

--- script.js
+++ script.js
@@ -38,3 +38,12 @@ for (var i = 0; i < miNombre.length; i++) {
     }
 }
 console.log(mapNombre);
+
+/*FASE 4
+Crea una altra array amb el teu cognom on cada posició correspongui a una lletra.
+Fusiona els dos arrays en un. A més, afegeix una posició amb un espai buit entre la primera i la segona. És a dir, partim de 'array name i surname i al acabar l’execució només tindrem una que es dirà fullName.
+*/
+var nombre = ["C", "R", "I", "S", "T", "I", "N", "A"];
+var apellido = ["M", "0", "T", "0", "C"];
+var fullName = nombre.concat(" ", apellido);
+console.log(fullName);
